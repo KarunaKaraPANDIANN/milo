58005dfbeb66f6ace76f4b84f68f6f515dff0165
karunakara pandian <karankaruna048@tuta.io>
2025-09-29 20:02:37 +0530

    milo

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.milo"
+    namespace = "io.github.karunakarapandiann.milo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
--- android/app/src/main/kotlin/com/example/milo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/milo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.milo
+package io.github.karunakarapandiann.milo
 
 import io.flutter.embedding.android.FlutterActivity
 
